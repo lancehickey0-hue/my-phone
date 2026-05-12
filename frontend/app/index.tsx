7fdf7cb421827d51d1a669f0c1515aa3c3694b09
lancehickey0-hue <lancehickey0@gmail.com>
2026-05-12 11:52:41 -0700

    Delete index.tsx

--- frontend/app/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { useAuthStore } from '../src/stores/authStore';
-import { Redirect } from 'expo-router';
-
-export default function Index() {
-  const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
-  
-  return isAuthenticated ? <Redirect href="/(tabs)" /> : <Redirect href="/login" />;
-}
