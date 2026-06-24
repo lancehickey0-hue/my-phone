6cddb14967bf5d3644cc5556ac25dfd17effa121
Lance <lancehickey0@gmail.com>
2026-06-23 17:46:22 -0700

    fix: use legacy ReactPackage for WakeWordModule registration, not TurboReactPackage

--- plugins/wake-word/WakeWordPackage.kt
+++ plugins/wake-word/WakeWordPackage.kt
@@ -1,25 +1,16 @@
 package com.myphone.app
 
-import com.facebook.react.TurboReactPackage
+import com.facebook.react.ReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
-import com.facebook.react.module.model.ReactModuleInfo
-import com.facebook.react.module.model.ReactModuleInfoProvider
+import com.facebook.react.uimanager.ViewManager
 
-class WakeWordPackage : TurboReactPackage() {
-    override fun getModule(name: String, reactContext: ReactApplicationContext): NativeModule? {
-        return if (name == WakeWordModule.NAME) WakeWordModule(reactContext) else null
+class WakeWordPackage : ReactPackage {
+    override fun createNativeModules(reactContext: ReactApplicationContext): MutableList<NativeModule> {
+        return mutableListOf(WakeWordModule(reactContext))
     }
 
-    override fun getReactModuleInfoProvider(): ReactModuleInfoProvider {
-        return ReactModuleInfoProvider {
-            mapOf(
-                WakeWordModule.NAME to ReactModuleInfo(
-                    WakeWordModule.NAME,
-                    WakeWordModule.NAME,
-                    false, false, false, false
-                )
-            )
-        }
+    override fun createViewManagers(reactContext: ReactApplicationContext): MutableList<ViewManager<*, *>> {
+        return mutableListOf()
     }
 }
